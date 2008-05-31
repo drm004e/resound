b077e45eb30a83a8877af28635dfb40837244784
Dave Moore <drm004e@gmail.com>
2008-05-31 15:10:38 +0000

    Improvements to example startup script

--- resound_server_start.sh
+++ resound_server_start.sh
@@ -5,7 +5,7 @@ hdspmixer &
 sleep 5
 
 # Run jackd and wait
-/usr/bin/jackd -R -dalsa -r44100 -p256 -n2 -D -Chw:0 -Phw:0 &
+/usr/bin/jackd -R -dalsa -r44100 -p512 -n2 -D -Chw:0 -Phw:0 &
 sleep 5
 
 # Run resound server with 24 I/O
@@ -13,9 +13,21 @@ resound_server -i8 -o24 -v &
 sleep 10
 
 # Make jack input connections
-jack_connect system:capture_7 Resound:Input0
-jack_connect system:capture_8 Resound:Input1
+#jack_connect system:capture_7 Resound:Input0
+#jack_connect system:capture_8 Resound:Input1
+
+# for loop version
+for ((i=0;i<8;i++)); do
+#sleep 0.2
+jack_connect system:capture_`expr $i + 1` Resound:Input$i
+done
 
 # Make jack output connections
-jack_connect Resound:Output0 system:playback_1
-jack_connect Resound:Output1 system:playback_2
+# for loop version
+for ((i=0;i<24;i++)); do
+#sleep 0.2
+jack_connect Resound:Output$i system:playback_`expr $i + 1`
+done
+
+
+
